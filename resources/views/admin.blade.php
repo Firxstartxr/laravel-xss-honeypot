83cc7103ddee082fbfb295e28bfac2a73b5da345
prokhor.ryazanov <prokhor.ryazanov@student.howest.be>
2022-12-06 17:26:53 +0100

    updated admin page style

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 
-    <table class="styled-table">
+    <h2 style="text-align: center">All Registered Users</h2>
+    <table style="width: 50rem; margin-left: auto; margin-right: auto">
         <thead>
         <tr>
             <th>Username</th>
